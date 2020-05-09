e1546124ccacf96a01972b35471c172b0b6c2e30
hahahasnoopy <maoleihahaha@gmail.com>
2020-05-09 17:42:07 +0800

    fix

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import xlsx = require("xlsx")
 import fs = require("fs")
 import { worker } from "./app"
+import Promise from "bluebird"
 
 interface Item{
   A:string
@@ -10,24 +11,16 @@ const workbook = xlsx.readFile("source.xls")
 const sheet = workbook.Sheets["符合不符合校内导师条件学生名单"]
 const arr  = xlsx.utils.sheet_to_json<Item>(sheet,{header:"A"}).map(a=>a.A)
 
-let p = Promise.resolve()
+arr.shift()//remove the first line
 
-arr.forEach((item,i)=>{
 
-  if(!item) {
-    return
+Promise.map(arr,item=>{
+  if(!item){
+    return Promise.resolve()
   }
-  p = p.then(()=>
-    {
-      console.log("initing",item)
-      return worker(item,"000000")
-    }
-  ).then(res=>{
-    fs.appendFile("result.txt",res,err=>{
-      console.log(err)
-    })
-    console.log("result",res,i)
-    return
-  })
-  }
-)
+  console.log("initing",item)
+  return worker(item,"000000").then(
+    res=>
+      fs.appendFile("result.txt",res,err=>{throw(err)})
+  )
+},{concurrency:1})
